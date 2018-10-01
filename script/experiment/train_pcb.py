ac47cc475a073fefce53f925b71e3f0c9452ef23
honglongcai <honglongcai@gmail.com>
2018-10-01 15:26:53 -0700

    10/01/2018

--- script/experiment/train_pcb.py
+++ script/experiment/train_pcb.py
@@ -439,7 +439,10 @@ def main():
 
       _, logits_list = model_w(ims_var)
       print('logits_list:', logits_list)
-      print('criterion:', [criterion(logits, labels_var) for logits in logits_list])
+      for logits in logits_list:
+        print(logits.size(),end=' ')
+      print()
+      print('criterion:', len([criterion(logits, labels_var) for logits in logits_list]))
       loss = torch.sum(torch.cat([criterion(logits, labels_var) for logits in logits_list]))
 
       optimizer.zero_grad()
