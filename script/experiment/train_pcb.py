4f64ffbe9e2482f48136a76b5cafbf1cc41c240a
honglongcai <honglongcai@gmail.com>
2018-10-04 11:39:39 -0700

    10/40/2018

--- script/experiment/train_pcb.py
+++ script/experiment/train_pcb.py
@@ -270,9 +270,10 @@ class ExtractFeature(object):
       local_feat_list, logits_list = self.model(ims)
     except:
       local_feat_list = self.model(ims)
-    print('local_feat_list_size:', local_feat_list.shape)
+    print('local_feat_list_size:', len(local_feat_list))
     feat = [lf.data.cpu().numpy() for lf in local_feat_list]
-    print('feat_size:', feat.shape)
+    print('feat_size:', len(feat))
+    print('feat_size:', feat[0].shape)
     feat = np.concatenate(feat, axis=1)
     print('feat_shape:', feat.shape)
 
